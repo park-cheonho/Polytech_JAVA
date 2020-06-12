18c76f5fb6b64733ad6577d5910962a9b00c5d4e
park-cheonho <cheonho08@gmail.com>
2020-06-12 21:59:33 +0900

    [ADD]20_06_12_과제

--- JAVA/src/kr/ac/kopo/day17/homework/AccountMain.java
+++ JAVA/src/kr/ac/kopo/day17/homework/AccountMain.java
@@ -16,24 +16,24 @@ class Account {
 
 	public synchronized void deposit(String name) {
 		
-		notifyAll();
+//		notifyAll();
 		//money = (r.nextInt(10) + 1) * 1000;
 		money = (int)(Math.random()*9000) + 1000;
 		System.out.println(name + " 손님이" + money + " 원 입금 하셨습니다.");
 		balance += money;
 		System.out.println("현재 잔액은 " + balance + "원입니다.");
-		try {
-			//notifyAll();
-			wait();
-			notifyAll();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			//notifyAll();
+//			wait();
+//			notifyAll();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	public synchronized void withdraw(String name) {
 		
-		notifyAll();
+//		notifyAll();
 		//money = (r.nextInt(10) + 1) * 1000;
 		money = (int)(Math.random()*9000) + 1000;
 		if (balance - money < 0 && balance < money) {
@@ -43,13 +43,13 @@ class Account {
 			balance -= money;
 		}
 		System.out.println("현재 잔액은 " + balance + "원입니다.");
-		try {
-			//notifyAll();
-			wait();
-			notifyAll();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			notifyAll();
+//			wait();
+//			notifyAll();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
 
@@ -65,7 +65,7 @@ class Users extends Thread {
 
 	@Override
 	public void run() {
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < 2; i++) {
 			account.deposit(name);
 			account.withdraw(name);
 		}
